df91a02261632471478c0bfe3d487d6fd9d33bdb
Priyanka-saw <priyankasaw51@gmail.com>
2025-07-13 15:42:18 +0530

    updated

--- project-2/src/components/ContactForm/ContactForm.jsx
+++ project-2/src/components/ContactForm/ContactForm.jsx
@@ -3,7 +3,7 @@ import styles from "./Contact.module.css";
 const ContactForm = () => {
   return (
     <section className={styles.container}>
-      <div className="{styles.contact_form}">
+      <div className={styles.contact_form}>
         <div className={styles.top_btn}>
           <Button text="SUPPORT CHAT" />
           <Button text="Call Support" />
@@ -11,10 +11,27 @@ const ContactForm = () => {
         <Button isOutline={true} text="Email Form" />
 
         <form action="">
+          <div className={styles.form_control}>
             <label htmlFor="name">Name</label>
+            <input type="text" name="name" />
+          </div>
+          <div className={styles.form_control}>
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" />
+          </div>
+          <div className={styles.form_control}>
+            <label htmlFor="text">Textarea</label>
+            <textarea name="text" rows={5} />
+          </div>
+
+          <div className="flex justify-end">
+            <Button text="Submit Button" />
+          </div>
         </form>
       </div>
-      <div className={styles.contact_image}></div>
+      <div className={styles.contact_image}>
+        <img src="./src/assets/box2-3.jpg " width={460} height={460} alt="" />
+      </div>
     </section>
   );
 };
